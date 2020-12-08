f3c47c72df5cd462da44c989d3db89df472e2456
duyphan <duy.phan@gnomons.com>
2020-12-08 12:36:25 +0700

    Set up test project

---
+++ projects/renesas/rx72n-envision-kit/e2studio/aws_tests/src/aws_tests.c
@@ -0,0 +1,17 @@
+/***********************************************************************
+*
+*  FILE        : aws_tests.c
+*  DATE        : 2020-12-08
+*  DESCRIPTION : Main Program
+*
+*  NOTE:THIS IS A TYPICAL EXAMPLE.
+*
+***********************************************************************/
+#include "r_smc_entry.h"
+
+void main(void);
+
+void main(void)
+{
+
+}
